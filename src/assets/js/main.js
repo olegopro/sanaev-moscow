aa700d8e7df5293f4818c7c09fcb89b98746fc57
olegopro <evilgazz@gmail.com>
2022-05-25 23:46:04 +0300

    Закончил предарительную сборку для десктопа

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,88 +3,188 @@
 	;('use strict')
 
 	$(document).ready(function () {
-		/* Header Sticky */
-		// const $window = $(window)
+		/* Select bar in Catalog page */
+		if (document.querySelector('select')) {
+			const select = document.querySelector('select')
 
-		// $window.on('scroll', function () {
-		// 	const scroll = $window.scrollTop()
-		// 	const logoDefault = $('.navbar-brand.logodefault img')
-		// 	if (scroll > 10) {
-		// 		$('header #navbar-main').addClass('scrollHeader').css({
-		// 			boxShadow: '0 0 6px rgba(0, 0, 0, 0.2)'
-		// 		})
-		// 		$('#navbar-main .nav-link, .btn-tel-header').css({
-		// 			color: 'black'
-		// 		})
+			select.addEventListener('blur', () => selectEvent())
+			select.addEventListener('click', () => selectEvent())
 
-		// 		$('.navbar-brand-logo').attr('src', `http://${document.location.host}/assets/images/logo-header-black.png`)
-		// 	} else {
-		// 		$('header #navbar-main').removeClass('scrollHeader').css({
-		// 			boxShadow: ''
-		// 		})
-		// 		$('#navbar-main .nav-link, .btn-tel-header').css({
-		// 			color: 'black'
-		// 		})
-
-		// 		$('.navbar-brand-logo').attr('src', `http://${document.location.host}/assets/images/logo-header-white.png`)
-		// 	}
-		// })
-
-		/* Range Slider jQuery UI */
-		$('#passengers-range').slider({
-			range: true,
-			min: 2,
-			max: 32,
-			values: [2, 32],
-			animate: 'fast',
-
-			slide: function (event, ui) {
-				$('.passengers-range-start').val(ui.values[0])
-				$('.passengers-range-end').val(ui.values[1])
+			const selectEvent = () => {
+				if (event.type == 'click') {
+					if (select.classList.contains('change')) {
+						select.classList.remove('change')
+					} else {
+						select.classList.add('change')
+					}
+				}
+				if (event.type == 'blur') {
+					select.classList.remove('change')
+				}
 			}
-		})
 
-		$('.passengers-range-start').val($('#passengers-range').slider('values', 0))
-		$('.passengers-range-end').val($('#passengers-range').slider('values', 1))
+			$('.select').each(function () {
+				const _this = $(this)
 
-		$('#year-range').slider({
-			range: true,
-			min: 1928,
-			max: 2022,
-			values: [1928, 2022],
-			animate: 'fast',
+				// console.log(_this)
+				const selectOption = _this.find('option')
+				const selectOptionLength = selectOption.length
+				const selectedOption = selectOption.filter(':selected')
+				const duration = 250 // длительность анимации
 
-			slide: function (event, ui) {
-				$('.year-range-start').val(ui.values[0])
-				$('.year-range-end').val(ui.values[1])
-			}
-		})
+				_this.hide()
+				_this.wrap('<div class="select"></div>')
 
-		$('.year-range-start').val($('#year-range').slider('values', 0))
-		$('.year-range-end').val($('#year-range').slider('values', 1))
+				$('<div>', {
+					class: 'new-select',
+					text: _this.children('option:disabled').text()
+				}).insertAfter(_this)
 
-		$('#price-range').slider({
-			range: true,
-			min: 1200,
-			max: 50000,
-			values: [1200, 50000],
-			animate: 'fast',
+				const selectHead = _this.next('.new-select')
+				$('<div>', {
+					class: 'new-select__list'
+				}).insertAfter(selectHead)
 
-			slide: function (event, ui) {
-				$('.price-range-start').val(ui.values[0])
-				$('.price-range-end').val(ui.values[1])
-			}
-		})
+				const selectList = selectHead.next('.new-select__list')
+				for (let i = 1; i < selectOptionLength; i++) {
+					$('<div>', {
+						class: 'new-select__item',
+						html: $('<span>', {
+							text: selectOption.eq(i).text()
+						})
+					})
+						.attr('data-value', selectOption.eq(i).val())
+						.appendTo(selectList)
+				}
 
-		$('.price-range-start').val($('#price-range').slider('values', 0))
-		$('.price-range-end').val($('#price-range').slider('values', 1))
+				const selectItem = selectList.find('.new-select__item')
+
+				selectList.slideUp(0)
+
+				let textArr = []
+
+				selectHead.on('click', function () {
+					if (!$(this).hasClass('on')) {
+						$(this).addClass('on')
+
+						selectList.fadeIn(duration)
+
+						console.log('selectList', selectList)
+
+						selectItem.on('click', function () {
+							let chooseItem = $(this).data('value')
+
+							if (!selectOption.filter(`option[value="${chooseItem}"]`).attr('selected')) {
+								// selectOption.parent().attr('selected', 'selected')
+
+								selectOption.filter(`option[value="${chooseItem}"]`).attr('selected', 'selected').prop('selected', true)
+
+								textArr.push($(this).find('span').css({ background: 'yellow' }).text())
+
+								$(this).addClass('check-box-item')
+
+								selectHead.text(function (index, text) {
+									selectHead.text('').append(textArr.join(', '))
+
+									console.log('textArr', textArr)
+								})
+							} else if (selectOption.filter(`option[value="${chooseItem}"]`).attr('selected')) {
+								$(this).find('span').css({ background: 'red' })
+
+								console.log($(this).attr('data-value'))
+
+								textArr.splice(textArr.indexOf($(this).attr('data-value')), 1)
+
+								selectOption.filter(`option[value="${chooseItem}"]`).removeAttr('selected').prop('selected', false)
+
+								$(this).removeClass('check-box-item')
+								$(this).find('span').css({ background: 'gray' })
+
+								if (textArr.length !== 0) {
+									selectHead.text('').append(textArr.join(', '))
+								} else {
+									selectHead.text(selectOption.filter(':disabled').val())
+									// selectOption.parent().removeAttr('selected')
+								}
+							}
+
+							// selectList.slideUp(duration)
+							// selectHead.removeClass('on')
+						})
+					} else {
+						$(this).removeClass('on')
+						selectList.fadeOut(duration / 1.6)
+					}
+				})
+			})
+
+			$('#showCarsPlaces').on('click', function (event) {
+				event.preventDefault()
+
+				if (!$(this).hasClass('on')) {
+					$(this).addClass('on')
+					$('#carsPlaces .input-group').fadeIn()
+				} else {
+					$(this).removeClass('on')
+					$('#carsPlaces .input-group').fadeOut()
+				}
+			})
+
+			$('#showCarsYears').on('click', function (event) {
+				event.preventDefault()
+
+				if (!$(this).hasClass('on')) {
+					$(this).addClass('on')
+					$('#carsYears .input-group').fadeIn()
+				} else {
+					$(this).removeClass('on')
+					$('#carsYears .input-group').fadeOut()
+				}
+
+				$('#carsYears .input-group').toggleClass('d-block')
+			})
+
+			$('#showCarsPrice').on('click', function (event) {
+				event.preventDefault()
+
+				if (!$(this).hasClass('on')) {
+					$('#carsPrice .input-group').fadeIn()
+					$(this).addClass('on')
+				} else {
+					$(this).removeClass('on')
+					$('#carsPrice .input-group').fadeOut()
+				}
+
+				$('#carsPrice .input-group').toggleClass('d-block')
+			})
+		}
 
 		/* Brazzers - Carousel */
-		// @ts-ignore
 		$('.images-toggle').brazzersCarousel()
 
 		/* FancyApp */
+		const carsTypesCatalog = document.querySelector('#cars-types-catalog')
+		if (carsTypesCatalog) {
+			new Carousel(carsTypesCatalog, {
+				Dots: false,
+				center: true,
+				infinite: false,
+				slidesPerPage: 2,
+				dragFree: true,
+				friction: 0.9,
+				l10n: {
+					NEXT: 'Следующий автомобиль',
+					PREV: 'Предыдущий  автомобиль',
+					GOTO: 'Вернуться к %d'
+				},
+				Navigation: {
+					prevTpl: '<svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 16L2 9L8 2" stroke="#1C2540" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+					nextTpl: '<svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 2L8 9L2 16" stroke="#1C2540" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+				}
+			})
+		}
 
+		/* Index page PremiumCars carousel */
 		const carouselPremiumCars = document.querySelector('#carousel-premium-cars')
 		if (carouselPremiumCars) {
 			new Carousel(carouselPremiumCars, {
@@ -100,6 +200,7 @@
 			})
 		}
 
+		/* Index page sportCars carousel */
 		const sportCars = document.querySelector('#carousel-sport-cars')
 		if (sportCars) {
 			new Carousel(sportCars, {
@@ -115,6 +216,7 @@
 			})
 		}
 
+		/* Index page CabrioletCars carousel */
 		const carouselCabrioletCars = document.querySelector('#carousel-cabriolet-cars')
 		if (carouselCabrioletCars) {
 			new Carousel(carouselCabrioletCars, {
@@ -130,6 +232,7 @@
 			})
 		}
 
+		/* Index page type Tab */
 		const typeTabCarouselFancy = document.querySelector('#type-tab-carousel-fancy')
 		if (typeTabCarouselFancy) {
 			new Carousel(typeTabCarouselFancy, {
@@ -144,7 +247,7 @@
 				}
 			})
 		}
-
+		/* Index page brands Tab */
 		const brandTabCarouselFancy = document.querySelector('#brand-tab-carousel-fancy')
 		if (brandTabCarouselFancy) {
 			new Carousel(brandTabCarouselFancy, {
@@ -160,6 +263,7 @@
 			})
 		}
 
+		/* Index page color Tab */
 		const colorTabCarouselFancy = document.querySelector('#color-tab-carousel-fancy')
 		if (colorTabCarouselFancy) {
 			new Carousel(colorTabCarouselFancy, {
@@ -175,6 +279,7 @@
 			})
 		}
 
+		/* Index page country Tab */
 		const countryTabCarouselFancy = document.querySelector('#country-tab-carousel-fancy')
 		if (countryTabCarouselFancy) {
 			new Carousel(countryTabCarouselFancy, {
@@ -190,11 +295,11 @@
 			})
 		}
 
-		/* Single car carousel */
+		/* Single car page carousel */
 		const singleCarCardCarousel = document.querySelector('#singleCarCardCarousel')
 		if (singleCarCardCarousel) {
 			// Initialise Carousel
-			const initSingleCarCardCarousel = new Carousel(singleCarCardCarousel, {
+			const initSingleCarCardCarousel = new Carousel(galleryVideoCardCarousel, {
 				Dots: false
 			})
 
@@ -223,6 +328,82 @@
 					}
 				}
 			})
+		}
+
+		/* Single car page carousel */
+		const galleryVideoCardCarousel = document.querySelector('#galleryVideoCardCarousel')
+		if (galleryVideoCardCarousel) {
+			// Initialise Carousel
+			const initGalleryVideoCardCarouse = new Carousel(galleryVideoCardCarousel, {
+				Dots: false
+			})
+
+			// Thumbnails
+			const galleryVideoCardThumbCarousel = new Carousel(document.querySelector('#galleryVideoCardThumbCarousel'), {
+				Sync: {
+					target: initGalleryVideoCardCarouse,
+					friction: 0
+				},
+				Dots: false,
+				Navigation: false,
+				center: true,
+				slidesPerPage: 1,
+				infinite: false
+			})
+
+			// Customize Fancybox
+			Fancybox.bind('[data-fancybox="gallery"]', {
+				Carousel: {
+					on: {
+						change: that => {
+							initGalleryVideoCardCarouse.slideTo(initGalleryVideoCardCarouse.findPageForSlide(that.page), {
+								friction: 0
+							})
+						}
+					}
+				}
+			})
+
+			Fancybox.bind('[data-fancybox="gallery"]', {
+				Image: {
+					zoom: false
+				}
+			})
+		}
+
+		/* Single service page carousel */
+		const singleServiceCardCarousel = document.querySelector('#singleServiceCardCarousel')
+		if (singleServiceCardCarousel) {
+			// Initialise Carousel
+			const initSingleServiceCardCarousel = new Carousel(singleServiceCardCarousel, {
+				Dots: false
+			})
+
+			// Thumbnails
+			const singleCarCardThumbCarousel = new Carousel(document.querySelector('#singleServiceCardThumbCarousel'), {
+				Sync: {
+					target: initSingleServiceCardCarousel,
+					friction: 0
+				},
+				Dots: false,
+				Navigation: false,
+				center: true,
+				slidesPerPage: 1,
+				infinite: false
+			})
+
+			// Customize Fancybox
+			Fancybox.bind('[data-fancybox="gallery"]', {
+				Carousel: {
+					on: {
+						change: that => {
+							initSingleServiceCardCarousel.slideTo(initSingleServiceCardCarousel.findPageForSlide(that.page), {
+								friction: 0
+							})
+						}
+					}
+				}
+			})
 
 			Fancybox.bind('[data-fancybox="gallery"]', {
 				Image: {
@@ -239,20 +420,6 @@
 					'close'
 				]
 			}
-		})
-
-		/* RelaxJS */
-		// new Rellax('.rellax')
-
-		// new Rellax('.rellax2', {
-		// 	center: true
-		// })
-
-		var image = document.getElementsByClassName('parallax-simple')
-		new simpleParallax(image, {
-			delay: 0.6,
-			transition: 'cubic-bezier(0,0,0,1)'
-			// customContainer: 'page-title-section'
 		})
 	})
 })(jQuery)
---
+++ src/html/pages/send.php
@@ -0,0 +1,6 @@
+<?php
+
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+
+var_dump($_POST['fio']);
